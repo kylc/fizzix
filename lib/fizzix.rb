5c28899b52e33b34c52cf22aea1790c331313433
Kyle Cesare <kcesare@gmail.com>
2013-04-22 21:32:20 -0700

    Add basic Particle implementation.

--- lib/fizzix.rb
+++ lib/fizzix.rb
@@ -1,2 +1,5 @@
+require_relative 'fizzix/vector'
+require_relative 'fizzix/particle'
+
 module Fizzix
 end
---
+++ lib/fizzix/particle.rb
@@ -0,0 +1,32 @@
+module Fizzix
+  GRAVITY = Fizzix::Vector.new(0, 0.02)
+
+  class Particle
+    attr_reader :pos
+
+    attr_reader :last_pos
+
+    # Public: Initialize a new Particle.
+    #
+    # pos - An initial Vector.
+    #
+    # Returns a new Particle.
+    def initialize(pos)
+      @pos = pos
+      @last_pos = pos
+    end
+
+    # Public: Determine the new position of the particle, taking into account
+    # gravity and inertia.
+    #
+    # Returns the new position.
+    def update!
+      velocity = @pos - @last_pos
+
+      @last_pos = @pos
+
+      @pos += Fizzix::GRAVITY
+      @pos += velocity
+    end
+  end
+end
