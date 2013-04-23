45dfcb3960ee518e8037f6faa69b19f0ffec3a91
Kyle Cesare <kcesare@gmail.com>
2013-04-22 21:13:37 -0700

    Basic Vector implementation and specs.

---
+++ lib/fizzix/vector.rb
@@ -0,0 +1,33 @@
+module Fizzix
+  class Vector
+    attr_reader :x, :y
+
+    def initialize(x, y)
+      @x, @y = x.to_f, y.to_f
+    end
+
+    def +(o)
+      Vector.new(self.x + o.x, self.y + o.y)
+    end
+
+    def -(o)
+      Vector.new(self.x - o.x, self.y - o.y)
+    end
+
+    def *(o)
+      Vector.new(self.x * o.x, self.y * o.y)
+    end
+
+    def /(o)
+      Vector.new(self.x / o.x, self.y / o.y)
+    end
+
+    def scale(c)
+      Vector.new(self.x * c, self.y * c)
+    end
+
+    def length
+      Math::sqrt(self.x ** 2 + self.y ** 2)
+    end
+  end
+end
